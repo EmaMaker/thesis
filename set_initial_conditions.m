500672a350264aece9ce2aa23ea481b4c8e99d88
EmaMaker <emamaker@tutanota.com>
2024-07-13 11:32:23 +0200

    more initial conditions

--- set_initial_conditions.m
+++ set_initial_conditions.m
@@ -1,8 +1,10 @@
 function x0 = set_initial_conditions(i)
 switch i
     case 0
-        x0 = [0; 0; 0]
+        x0 = [0; 0; 0];
     case 1
-        x0 = [0; 0; PI]
+        x0 = [0; 0; pi];
+    case 2
+        x0 = [0, 0; pi/6];
 end
 end
