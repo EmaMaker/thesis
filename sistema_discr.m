30540c1bc88431298497ee2a14f82d48e6fabb8b
EmaMaker <emamaker@tutanota.com>
2024-09-06 15:49:04 +0200

    sistema_discr: better comments

--- sistema_discr.m
+++ sistema_discr.m
@@ -1,5 +1,10 @@
 function dq = sistema_discr(t, q, u_discr, sim_data)
     theta = q(3);
-    G_q = [cos(theta), 0; sin(theta), 0; 0, 1];
-    dq = G_q*diffdrive_to_uni(u_discr, sim_data);
+    %dq = T(q)*W*u
+
+    % T_q (unicycle)
+    T_q = [cos(theta), 0; sin(theta), 0; 0, 1];
+    % W*u
+    Wu = diffdrive_to_uni(u_discr, sim_data);
+    dq = T_q*Wu;
 end
